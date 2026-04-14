1e05a182ff380d0033a9e4be68af7e811bd3bc8b
Ayush Kumar Nath <ayushkumar.nath25@gmail.com>
2026-04-14 11:32:28 +0000

    editor styling done

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,24 @@
   import { v4 as uuidv4 } from "uuid";
   import { Button } from "$lib/components/ui/button/index.js";
   import { Input } from "$lib/components/ui/input/index.js";
-  import { PanelRight, Search } from "@lucide/svelte";
+  import {
+    Bold,
+    Code,
+    Heading1,
+    Heading2,
+    Italic,
+    Link,
+    List,
+    ListOrdered,
+    PanelRight,
+    Search,
+    Strikethrough,
+    Underline,
+  } from "@lucide/svelte";
+  import { onMount } from "svelte";
+  import { Editor } from "@tiptap/core";
+  import { StarterKit } from "@tiptap/starter-kit";
+  import { Placeholder } from "@tiptap/extensions";
 
   interface Note {
     id: string;
@@ -45,6 +62,25 @@
   let isSidebarClosed: boolean = $state(false);
 
   let query = $state("");
+
+  let bodyField: HTMLElement | undefined = $state();
+  let editorState: { editor: Editor | null } = $state({ editor: null });
+
+  onMount(() => {
+    editorState.editor = new Editor({
+      element: bodyField,
+      extensions: [
+        StarterKit,
+        Placeholder.configure({
+          placeholder: "Body goes here...",
+        }),
+      ],
+      // content: `Hello World`,
+      onTransaction: ({ editor }) => {
+        editorState = { editor };
+      },
+    });
+  });
 
   function filterNotes(): void {
     if (!query) {
@@ -96,12 +132,12 @@
   }
 </script>
 
-<div class="max-w-384 h-screen mx-auto overflow-hidden 2xl:border-x">
-  <section
-    class="sidebar relative {isSidebarClosed
-      ? 'w-20'
-      : 'w-xs'} h-full border-r p-4"
-  >
+<div
+  class="grid {isSidebarClosed
+    ? 'grid-cols-[80px_1fr]'
+    : 'grid-cols-[20rem_1fr]'} max-w-384 h-screen mx-auto overflow-hidden 2xl:border-x"
+>
+  <section class="sidebar relative h-full border-r p-4">
     <header
       class="flex items-center {isSidebarClosed
         ? 'justify-center'
@@ -170,6 +206,207 @@
       <p class={isSidebarClosed ? "text-sm" : ""}>Work in progress</p>
     </div>
   </section>
+
+  <section class="editor py-4 px-8">
+    <header class="flex items-center gap-4 mb-12">
+      <div class="toolbar">
+        <ul class="flex items-center gap-2">
+          <li>
+            <Button
+              size="icon"
+              variant="outline"
+              class={editorState.editor?.isActive("bold")
+                ? "bg-gray-800 text-white outline-transparent hover:bg-gray-600 hover:text-white hover:outline-transparent"
+                : ""}
+              onclick={() =>
+                editorState.editor?.chain().focus().toggleBold().run()}
+              disabled={!editorState.editor
+                ?.can()
+                .chain()
+                .focus()
+                .toggleBold()
+                .run()}
+            >
+              <Bold />
+            </Button>
+          </li>
+          <li>
+            <Button
+              size="icon"
+              variant="outline"
+              class={editorState.editor?.isActive("italic")
+                ? "bg-gray-800 text-white outline-transparent hover:bg-gray-600 hover:text-white hover:outline-transparent"
+                : ""}
+              onclick={() =>
+                editorState.editor?.chain().focus().toggleItalic().run()}
+              disabled={!editorState.editor
+                ?.can()
+                .chain()
+                .focus()
+                .toggleItalic()
+                .run()}
+            >
+              <Italic />
+            </Button>
+          </li>
+          <li>
+            <Button
+              size="icon"
+              variant="outline"
+              class={editorState.editor?.isActive("underline")
+                ? "bg-gray-800 text-white outline-transparent hover:bg-gray-600 hover:text-white hover:outline-transparent"
+                : ""}
+              onclick={() =>
+                editorState.editor?.chain().focus().toggleUnderline().run()}
+              disabled={!editorState.editor
+                ?.can()
+                .chain()
+                .focus()
+                .toggleUnderline()
+                .run()}
+            >
+              <Underline />
+            </Button>
+          </li>
+          <li>
+            <Button
+              size="icon"
+              variant="outline"
+              class={editorState.editor?.isActive("strike")
+                ? "bg-gray-800 text-white outline-transparent hover:bg-gray-600 hover:text-white hover:outline-transparent"
+                : ""}
+              onclick={() =>
+                editorState.editor?.chain().focus().toggleStrike().run()}
+              disabled={!editorState.editor
+                ?.can()
+                .chain()
+                .focus()
+                .toggleStrike()
+                .run()}
+            >
+              <Strikethrough />
+            </Button>
+          </li>
+          <li>
+            <Button
+              size="icon"
+              variant="outline"
+              class={editorState.editor?.isActive("bulletList")
+                ? "bg-gray-800 text-white outline-transparent hover:bg-gray-600 hover:text-white hover:outline-transparent"
+                : ""}
+              onclick={() =>
+                editorState.editor?.chain().focus().toggleBulletList().run()}
+              disabled={!editorState.editor
+                ?.can()
+                .chain()
+                .focus()
+                .toggleBulletList()
+                .run()}
+            >
+              <List />
+            </Button>
+          </li>
+          <li>
+            <Button
+              size="icon"
+              variant="outline"
+              class={editorState.editor?.isActive("orderedList")
+                ? "bg-gray-800 text-white outline-transparent hover:bg-gray-600 hover:text-white hover:outline-transparent"
+                : ""}
+              onclick={() =>
+                editorState.editor?.chain().focus().toggleOrderedList().run()}
+              disabled={!editorState.editor
+                ?.can()
+                .chain()
+                .focus()
+                .toggleOrderedList()
+                .run()}
+            >
+              <ListOrdered />
+            </Button>
+          </li>
+          <li>
+            <Button
+              size="icon"
+              variant="outline"
+              class={editorState.editor?.isActive("heading", { level: 1 })
+                ? "bg-gray-800 text-white outline-transparent hover:bg-gray-600 hover:text-white hover:outline-transparent"
+                : ""}
+              onclick={() =>
+                editorState.editor
+                  ?.chain()
+                  .focus()
+                  .toggleHeading({ level: 1 })
+                  .run()}
+              disabled={!editorState.editor
+                ?.can()
+                .chain()
+                .focus()
+                .toggleHeading({ level: 1 })
+                .run()}
+            >
+              <Heading1 />
+            </Button>
+          </li>
+          <li>
+            <Button
+              size="icon"
+              variant="outline"
+              class={editorState.editor?.isActive("heading", { level: 2 })
+                ? "bg-gray-800 text-white outline-transparent hover:bg-gray-600 hover:text-white hover:outline-transparent"
+                : ""}
+              onclick={() =>
+                editorState.editor
+                  ?.chain()
+                  .focus()
+                  .toggleHeading({ level: 2 })
+                  .run()}
+              disabled={!editorState.editor
+                ?.can()
+                .chain()
+                .focus()
+                .toggleHeading({ level: 2 })
+                .run()}
+            >
+              <Heading2 />
+            </Button>
+          </li>
+          <li>
+            <Button
+              size="icon"
+              variant="outline"
+              class={editorState.editor?.isActive("codeBlock")
+                ? "bg-gray-800 text-white outline-transparent hover:bg-gray-600 hover:text-white hover:outline-transparent"
+                : ""}
+              onclick={() =>
+                editorState.editor?.chain().focus().toggleCodeBlock().run()}
+              disabled={!editorState.editor
+                ?.can()
+                .chain()
+                .focus()
+                .toggleCodeBlock()
+                .run()}
+            >
+              <Code />
+            </Button>
+          </li>
+        </ul>
+      </div>
+      <!-- <div class="note-save-status"></div> -->
+    </header>
+
+    <div class="editor-fields">
+      <div class="title-field mb-12">
+        <input
+          class="text-xl w-full"
+          id="editor-title-input"
+          type="text"
+          placeholder="Title goes here..."
+        />
+      </div>
+      <div class="body-field" bind:this={bodyField}></div>
+    </div>
+  </section>
 </div>
 
 <style>
@@ -178,5 +415,49 @@
     -webkit-line-clamp: 3;
     -webkit-box-orient: vertical;
     overflow: hidden;
+  }
+
+  #editor-title-input {
+    border: 0;
+    outline: 0;
+  }
+
+  :global(.tiptap) {
+    outline: 0;
+  }
+
+  :global(.tiptap p.is-editor-empty:first-child::before) {
+    color: #848484;
+    content: attr(data-placeholder);
+    float: left;
+    height: 0;
+    pointer-events: none;
+  }
+
+  :global(.tiptap pre) {
+    padding: 0.5rem;
+    color: #fff;
+    border-radius: 0.5rem;
+    background-color: #2e2b29;
+  }
+
+  :global(.tiptap ul) {
+    list-style-type: disc;
+    padding-left: 1.5rem;
+  }
+
+  :global(.tiptap ol) {
+    list-style-type: decimal;
+    padding-left: 1.5rem;
+  }
+
+  :global(.tiptap h1) {
+    font-size: 2rem;
+    font-weight: bold;
+  }
+
+  :global(.tiptap h2) {
+    font-size: 1.5rem;
+    font-weight: bold;
   }
 </style>
